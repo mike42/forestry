b669208e8822274cf7fd9a120bc4091714ece788
Michael Billington <michael.billington@gmail.com>
2014-05-24 19:46:30 +1000

    fix code block

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <getopt.h>
 #include "SimulationDriver.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
+
+
+
+
     SimulationDriver *sim = new SimulationDriver(0.5);
     sim -> run();
     delete sim;
